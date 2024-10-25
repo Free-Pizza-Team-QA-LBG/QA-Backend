8757f5a3c7f578bc41f81a5617f44cec8ea1c3cf
BenSnellgrove <ben.e.snellgrove@gmail.com>
2024-10-25 10:49:01 +0100

    It runs.

---
+++ src/main/java/com/pizza/PizzaBackend.java
@@ -0,0 +1,14 @@
+package com.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaBackend {
+
+    public static void main(String[] args) {
+        System.out.printf("Loading pizza backend\n\n");
+        SpringApplication.run(PizzaBackend.class, args);
+
+    }
+}
