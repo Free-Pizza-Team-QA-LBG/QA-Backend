961061213547393b5b7af464b673ac96c9100eba
Jordan Taylor <nadroj.taylor99@gmail.com>
2024-10-28 15:47:08 +0000

    Temp test cases

--- src/test/java/com/pizza/PizzaBackendTests.java
+++ src/test/java/com/pizza/PizzaBackendTests.java
@@ -1,13 +1,29 @@
 package com.pizza;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
 class PizzaBackendTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    //jdt test functions
+    @Test
+    void alwaysTrue(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void alwaysFalse(){
+        Assertions.assertTrue(false);
     }
 
 }
