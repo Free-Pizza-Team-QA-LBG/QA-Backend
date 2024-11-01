6eb4b94478455e94a5da6396c770fef26f04c23f
Jordan Taylor <nadroj.taylor99@gmail.com>
2024-11-01 11:52:53 +0000

    more webstest

--- src/test/java/com/pizza/PizzaBackendTests.java
+++ src/test/java/com/pizza/PizzaBackendTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.assertTrue;
 
-//tst6
+//tstMORE
 
 //@SpringBootTest
 class PizzaBackendTests {
