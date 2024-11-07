d3e5324bb8b3f324566400bf17e3442be0f14917
BenSnellgrove <ben.e.snellgrove@gmail.com>
2024-11-07 11:19:25 +0000

    Add basic unit test

--- src/test/java/com/pizza/PizzaBackendTests.java
+++ src/test/java/com/pizza/PizzaBackendTests.java
@@ -2,10 +2,19 @@ package com.pizza;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PizzaBackendTests {
 
+    /**
+     * A test for the CI/CD pipeline to ingest
+     * If this fails, things have gone horribly wrong
+     */
     @Test
-    void contextLoads() {
+    void pipelineTest() {
+
+        assertTrue(true);
+
     }
 
 }
