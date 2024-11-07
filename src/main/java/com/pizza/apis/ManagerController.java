138269e8ad7b14fa6253ab589e0c93fb07a6e50e
BenSnellgrove <ben.e.snellgrove@gmail.com>
2024-11-07 12:14:31 +0000

    Add get all endpoint

--- src/main/java/com/pizza/apis/ManagerController.java
+++ src/main/java/com/pizza/apis/ManagerController.java
@@ -1,15 +1,46 @@
 package com.pizza.apis;
 
 import com.pizza.services.ManagerService;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/role")
+@RequestMapping("/api/v1/manager")
 public class ManagerController {
 
     @Autowired
     private ManagerService managerService;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getAllEmployees() {
+
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray managersArray = new JSONArray();
+
+        managerService.getAll().forEach(manager -> managersArray.put(manager.toJSON()));
+        jsonResponse.put("data", managersArray);
+
+        return ResponseEntity.ok(jsonResponse.toString());
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getByManagerID(@PathVariable int id) {
+
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray managersArray = new JSONArray();
+
+        managerService.getByManagerId(id).forEach(manager -> managersArray.put(manager.toJSON()));
+        jsonResponse.put("data", managersArray);
+
+        return ResponseEntity.ok(jsonResponse.toString());
+    }
 
 }
--- src/main/java/com/pizza/services/ManagerService.java
+++ src/main/java/com/pizza/services/ManagerService.java
@@ -1,14 +1,30 @@
 package com.pizza.services;
 
+import com.pizza.entities.Manager;
 import com.pizza.repos.ManagerRepo;
-import com.pizza.repos.RoleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ManagerService {
 
     @Autowired
     private ManagerRepo managerRepo;
+
+    public List<Manager> getAll() {
+        return managerRepo.findAll();
+    }
+
+    public List<Manager> getByManagerId(int id) {
+        List<Manager> out = new ArrayList<>();
+
+        if (managerRepo.findById(id).isPresent())
+            out.add(managerRepo.findById(id).get());
+
+        return out;
+    }
 
 }
