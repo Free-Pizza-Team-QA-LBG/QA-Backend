a2320e1f566b45d221567aa7227e1f7805663604
Ben <94117159+BenSnellgrove@users.noreply.github.com>
2024-11-07 15:18:02 +0000

    release: get endpoints for manager / managee (#37)
    * Change Dockerfile to only run, build will happen in Jenkins pipeline
    
    * release: new tables have been added (#31)
    
    * Add Role and Manager tables
    Fixed a typo in method name (getLastLame -> getLastName)
    
    * Manager and Role Endpoints setup
    
    * Add basic unit test
    
    * feat: Add default role (#34)
    
    Add json protection against existing null roles
    
    * feat: manager / managed employee GET endpoints (#36)
    
    * Add get all endpoint
    
    * Get mappings for manager and managed employee

--- src/main/java/com/pizza/apis/ManagerController.java
+++ src/main/java/com/pizza/apis/ManagerController.java
@@ -1,15 +1,59 @@
 package com.pizza.apis;
 
+import com.pizza.entities.ManagerID;
 import com.pizza.services.ManagerService;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/role")
+@RequestMapping("/api/v1/manager")
 public class ManagerController {
 
     @Autowired
     private ManagerService managerService;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getAllEmployees() {
+
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray managersArray = new JSONArray();
+
+        managerService.getAll().forEach(manager -> managersArray.put(manager.toJSON()));
+        jsonResponse.put("data", managersArray);
+
+        return ResponseEntity.ok(jsonResponse.toString());
+    }
+
+    @GetMapping(value = "/manages/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getByManagerID(@PathVariable int id) {
+
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray managersArray = new JSONArray();
+
+        managerService.getByManagerId(id).forEach(manager -> managersArray.put(manager.toJSON()));;
+        jsonResponse.put("data", managersArray);
+
+        return ResponseEntity.ok(jsonResponse.toString());
+    }
+
+    @GetMapping(value = "/managed/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getByEmployeeID(@PathVariable int id) {
+
+        JSONObject jsonResponse = new JSONObject();
+        JSONArray employeesArray = new JSONArray();
+
+        managerService.getByEmployeeId(id).forEach(employee -> employeesArray.put(employee.toJSON()));;
+        jsonResponse.put("data", employeesArray);
+
+        return ResponseEntity.ok(jsonResponse.toString());
+    }
+
 
 }
--- src/main/java/com/pizza/services/ManagerService.java
+++ src/main/java/com/pizza/services/ManagerService.java
@@ -1,14 +1,45 @@
 package com.pizza.services;
 
+import com.pizza.entities.Manager;
 import com.pizza.repos.ManagerRepo;
-import com.pizza.repos.RoleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ManagerService {
 
     @Autowired
     private ManagerRepo managerRepo;
+
+    public List<Manager> getAll() {
+        return managerRepo.findAll();
+    }
+
+    public List<Manager> getByManagerId(int id) {
+        List<Manager> out = new ArrayList<>();
+
+        for (Manager manager : managerRepo.findAll()) {
+            if (manager.getId().getManager().getId() == id) {
+                out.add(manager);
+            }
+        }
+
+        return out;
+    }
+
+    public List<Manager> getByEmployeeId(int id) {
+        List<Manager> out = new ArrayList<>();
+
+        for (Manager manager : managerRepo.findAll()) {
+            if (manager.getId().getEmployee().getId() == id) {
+                out.add(manager);
+            }
+        }
+
+        return out;
+    }
 
 }
